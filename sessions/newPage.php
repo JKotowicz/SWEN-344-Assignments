f7c1adace9e2e83c0934150bf9ba1cb1b42acdb0
JKotowicz <jtk7412@rit.edu>
2017-04-29 13:00:47 -0400

    added form for favoirte food and color without ajax

--- sessions/newPage.php
+++ sessions/newPage.php
@@ -19,26 +19,40 @@ if ($_SESSION["username"] != "") {
 ?>
 
 <br /><br />
+<p>Enter your favorite color and food.</p>
+<form role = "form" action = "" method = "post">
+    <input type = "text" name = "favColor"></br>
+    <input type = "text" name = "favFood"></br>
+    <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "submit">Submit</button>
+</form>
+
+<br />
+<?php 
+//read the session for color and favorite food
+if (isset($_POST['favColor'])) {
+	$_SESSION['favColor'] = $_POST['favColor'];
+}
+if (isset($_POST['favFood'])) {
+	$_SESSION['favFood'] = $_POST['favFood'];
+}
+?>
+<p>
+	Your favorite color is: <?php echo $_SESSION['favColor'];?>
+	<br /><br />
+	Your favorite food is: <?php echo $_SESSION['favFood'];?>
+</p>
+
+<br /><br />
 <a href="clearsessions.php">Clear Sessions</a>
-
-
-
 
 <?php  
 }else{
 ?>
-
 	You are not a valid user
-
 
 	<?php
 }
-
-
 ?>
-
-
-
 
 </body>
 </html>
