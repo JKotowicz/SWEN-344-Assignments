0697fcce1a4a5daea3bb65719349a6d54e816526
JKotowicz <jtk7412@rit.edu>
2017-04-29 15:53:30 -0400

    fixing

--- sessions/newPage.php
+++ sessions/newPage.php
@@ -21,39 +21,37 @@ if ($_SESSION["username"] != "") {
 <br /><br />
 <p>Enter your favorite color and food.</p>
 <form role = "form" action = "" method = "post">
-    Color: <input type = "text" name = "favColor"></br>
-    Food:  <input type = "text" name = "favFood"></br>
+    Color: <input type = "text" name = "favColor" id="favColor"></br>
+    Food:  <input type = "text" name = "favFood" id="favFood"></br>
     <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "submit">Submit</button>
 </form>
 
 <br />
-<?php 
-// read the session for color and favorite food
-if (isset($_POST['favColor'])) {
-	$_SESSION['favColor'] = $_POST['favColor'];
-	localStorage.setItem("favColor", $_POST['favColor']);
-}
-if (isset($_POST['favFood'])) {
-	$_SESSION['favFood'] = $_POST['favFood'];
-	localStorage.setItem("favFood", $_POST['favFood']);
-}
-?>
 
 <script>
 // Store
-	localStorage.setItem("favColor", $_POST['favColor']);
+	var inputColor = document.getElementById("favColor");
+	localStorage.setItem("color", inputEmail.value);
+	var fColor = localStorage.getItem("color");
+
+	var inputColor = document.getElementById("favFood");
+	localStorage.setItem("food", inputEmail.value);
+	var fFood = localStorage.getItem("food");
+
+	document.querySelector('.result').innerHTML = fColor;
+	document.querySelector('.result2').innerHTML = fFood;
     //localStorage.setItem("favFood", $_SESSION['favFood']);
     // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("favColor");
+    //document.getElementById("result").innerHTML = localStorage.getItem("favColor");
     //document.getElementById("result2").innerHTML = localStorage.getItem("favFood");
 //    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
     //document.getElementById("result2").innerHTML = "Sorry, your browser does not support Web Storage...";
 </script>
 
 <p>
-	Your favorite color is: <div id="result"></div>
+	Your favorite color is: <div class="result"></div>
 	<br /><br />
-	Your favorite food is: <div id=""></div>
+	Your favorite food is: <div id="result2"></div>
 </p>
 
 <br /><br />
