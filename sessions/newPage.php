c1367de23884d9236f6c748881948981a3a26241
JKotowicz <jtk7412@rit.edu>
2017-04-29 16:01:54 -0400

    fixing

--- sessions/newPage.php
+++ sessions/newPage.php
@@ -21,31 +21,30 @@ if ($_SESSION["username"] != "") {
 <br /><br />
 <p>Enter your favorite color and food.</p>
 <form role = "form" action = "" method = "post">
-    Color: <input type = "text" name = "favColor" id="favColor"></br>
-    Food:  <input type = "text" name = "favFood" id="favFood"></br>
-    <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "submit">Submit</button>
+    Color: <input type = "text" name = "favColor" id="favColor" required></br>
+    Food:  <input type = "text" name = "favFood" id="favFood" required></br>
+    <button class = "sub" type = "submit" name = "submit">Submit</button>
 </form>
 
 <br />
 
 <script>
 // Store
-	var inputColor = document.getElementById("favColor");
-	localStorage.setItem("color", inputColor.value);
-	var fColor = localStorage.getItem("color");
+$(document).ready( function() {
+     $('.sub').on('click', function() {
+ 
+		var inputColor = document.getElementById("favColor");
+		localStorage.setItem("color", inputColor.value);
+		var fColor = localStorage.getItem("color");
 
-	var inputFood = document.getElementById("favFood");
-	localStorage.setItem("food", inputFood.value);
-	var fFood = localStorage.getItem("food");
+		var inputFood = document.getElementById("favFood");
+		localStorage.setItem("food", inputFood.value);
+		var fFood = localStorage.getItem("food");
 
-	document.querySelector('.result').innerHTML = fColor;
-	document.querySelector('.result2').innerHTML = fFood;
-    //localStorage.setItem("favFood", $_SESSION['favFood']);
-    // Retrieve
-    //document.getElementById("result").innerHTML = localStorage.getItem("favColor");
-    //document.getElementById("result2").innerHTML = localStorage.getItem("favFood");
-//    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-    //document.getElementById("result2").innerHTML = "Sorry, your browser does not support Web Storage...";
+		document.querySelector('.result').innerHTML = fColor;
+		document.querySelector('.result2').innerHTML = fFood;
+	}
+}); 
 </script>
 
 <p>
