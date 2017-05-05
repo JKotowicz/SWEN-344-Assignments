881e297b54a8026c6f9409527c60afc2ec9c45aa
JKotowicz <jtk7412@rit.edu>
2017-05-05 19:15:56 -0400

    ficing stuff

--- feed/signupInfo.php
+++ feed/signupInfo.php
@@ -2,9 +2,7 @@
 if (isset($_POST['Username'], $_POST['signup-pWord'], $_POST['confirm-pWord'])) {
 	if ($_POST['signup-pWord'] == $_POST['confirm-pWord']) {
 		print_r($_POST);
-		$('form.signup').trigger("reset");
-		alert("You're signed in!");
 	} else {
-		alert("You're passwords don't match.")
+		print_r("You're passwords don't match.");
 	}
 }
